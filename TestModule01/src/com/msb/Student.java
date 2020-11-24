a95eed11eb0e9bc23dfb8f99f05dfe3c69d3973f
wanghaoran <1518045665@qq.com>
2020-11-24 15:18:37 +0800

    在Student类中增加了age,name属性

---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: wanghaoran
+ * @Date: 2020/11/24-15:12
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
